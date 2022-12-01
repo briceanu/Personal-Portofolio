02955dd6d22a0a3cf4a7c08edd6fa97d19a5c8b3
Teodor <ursus_24@yahoo.com>
2022-12-01 02:04:21 +0200

    main styled

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import style from '../style/style.scss';
+import github from '../assets/github.svg';
 const btn = document.querySelector('.btn_container');
 const window1 = document.body;
 btn.addEventListener('click', () => {
